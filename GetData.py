587f4ba2588e4e7ed5c8cfa698aff9dba27ff3eb
yosif <yosifyosifov0@gmail.com>
2022-03-01 18:37:22 +0200

    SMA changes

--- GetData.py
+++ GetData.py
@@ -13,24 +13,67 @@ from dateutil.relativedelta import relativedelta
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = sqlalchemy.create_engine('sqlite:///BTCUSDTstream.db')
+
 def getHistoricalData4Hours(token_name):
     client = Client("CjhmrIu0k3VdrvML36fnYdx04kyA1u02QbJDUzirFfvHLx8wSEiiiRhRdrTzxika", "5faxSLXjUKKHsHT32kjkL78O75LzkeGI3c8Yp9awSQK7DMOlLoeMb990G1abbwrq")
     bsm = BinanceSocketManager(client)
     socket = bsm.trade_socket(token_name)
-    for currKline in client._historical_klines_generator(token_name, client.KLINE_INTERVAL_1MINUTE, str(datetime.now() - relativedelta(hours=8))):
-        frame = createFrame(currKline, "1M")
+    firstRun = True
+    for currKline in client._historical_klines_generator(token_name, client.KLINE_INTERVAL_1MINUTE, str(datetime.now() - relativedelta(hours=5))):
+        SMA5, SMA15, SMA50, SMA100 = 0, 0, 0, 0
+        if not firstRun:
+            df = pd.read_sql('BTCUSDT', engine)
+            size = len(df) - 1
+            if size > 5:
+                for i in range (size - 5, size):
+                    SMA5 += df.ClosePrice[i]
+                SMA5 /= 5
+
+            if size > 15:
+                for i in range (size - 15, size):
+                    SMA15 += df.ClosePrice[i]
+                SMA15 /= 15
+
+            if size > 50:
+                for i in range (size - 50, size):
+                    SMA50 += df.ClosePrice[i]
+                SMA50 /= 50
+
+            if size > 100:
+                for i in range (size - 100, size):
+                    SMA100 += df.ClosePrice[i]
+                SMA100 /= 100
+        firstRun = False
+        frame = createFrame(currKline, [SMA5, SMA15, SMA50, SMA100])
         frame.to_sql(token_name, engine, if_exists='append', index=False)
         print (frame)
 
-async def getCurrentData4Hours(token_name):
+async def getCurrentData4Hours(token_name, df):
     asyncClient = await AsyncClient.create("CjhmrIu0k3VdrvML36fnYdx04kyA1u02QbJDUzirFfvHLx8wSEiiiRhRdrTzxika", "5faxSLXjUKKHsHT32kjkL78O75LzkeGI3c8Yp9awSQK7DMOlLoeMb990G1abbwrq")
     bsm = BinanceSocketManager(asyncClient)
     socket = bsm.trade_socket(token_name)
 
     await asyncio.sleep(60)
     while True:
+        dataSize = len(df)
+        SMA5, SMA15, SMA50, SMA100 = 0, 0, 0, 0
+
+        for i in range (dataSize, dataSize - 100):
+            if i > dataSize - 5:
+                SMA5 += df.ClosePrise[i]
+            if i > dataSize - 15:
+                    SMA15 += df.ClosePrise[i]
+            if i > dataSize - 50:
+                SMA50 += df.ClosePrise[i]
+            SMA100 += df.ClosePrise[i]
+        SMA5 /= 5
+        SMA15 /= 15
+        SMA50 /= 50
+        SMA100 /= 100
+
         msg = await asyncClient.get_klines(symbol=token_name, interval=asyncClient.KLINE_INTERVAL_1MINUTE, limit=1)
-        frame = createFrame(msg[0], "1M")
+        frame = createFrame(msg[0], [SMA5, SMA15, SMA50, SMA100])
         frame.to_sql(token_name, engine, if_exists='append', index=False)
         print(frame)
         await asyncio.sleep(60)
@@ -40,7 +83,7 @@ def getHistoricalData1Day(token_name):
     bsm = BinanceSocketManager(client)
     socket = bsm.trade_socket(token_name)
     for currKline in client._historical_klines_generator(token_name, client.KLINE_INTERVAL_1DAY, str(datetime.now() - relativedelta(years=1))):
-        frame = createFrame(currKline, "1D")
+        frame = createFrame(currKline)
         frame.to_sql(token_name, engine, if_exists='append', index=False)
         print (frame)
 
@@ -52,32 +95,70 @@ async def getCurrentData1Day(token_name):
     await asyncio.sleep(86400)
     while True:
         msg = await asyncClient.get_klines(symbol=token_name, interval=asyncClient.KLINE_INTERVAL_1DAY, limit=1)
-        frame = createFrame(msg[0], "1D")
+        frame = createFrame(msg[0])
         frame.to_sql(token_name, engine, if_exists='append', index=False)
         print(frame)
         await asyncio.sleep(86400)
 
-def createFrame(msg, candleSize):
-    values = ["BTCUSDT", msg[0], msg[1], msg[2], msg[3], msg[4], msg[6], candleSize]
-    titles = ["Symbol", "OpenTime", "OpenPrice", "High", "Low", "ClosePrice", "CloseTime", "CandleSize"]
+def createFrame(msg, SMAs):
+    values = []
+    titles = []
+    if len(SMAs) > 0:
+        values = ["BTCUSDT", msg[0], msg[1], msg[2], msg[3], msg[4], msg[6], SMAs[0], SMAs[1], SMAs[2], SMAs[3]]
+        titles = ["Symbol", "OpenTime", "OpenPrice", "High", "Low", "ClosePrice", "CloseTime", "SMA5", "SMA15", "SMA50", "SMA100"]
+    else:
+        values = ["BTCUSDT", msg[0], msg[1], msg[2], msg[3], msg[4], msg[6]]
+        titles = ["Symbol", "OpenTime", "OpenPrice", "High", "Low", "ClosePrice", "CloseTime"]
     info = dict(zip(titles, values))
     frame = pd.DataFrame([info])
-    frame = frame.loc[:,["Symbol", "OpenTime", "OpenPrice", "High", "Low", "ClosePrice", "CloseTime", "CandleSize"]]
-    frame.columns = ['Symbol', 'OpenTime', 'OpenPrice', 'High', 'Low', 'ClosePrice', 'CloseTime', 'CandleSize']
+    if len(SMAs) > 0:
+        frame = frame.loc[:,["Symbol", "OpenTime", "OpenPrice", "High", "Low", "ClosePrice", "CloseTime", "SMA5", "SMA15", "SMA50", "SMA100"]]
+        frame.columns = ['Symbol', 'OpenTime', 'OpenPrice', 'High', 'Low', 'ClosePrice', 'CloseTime', 'SMA5', 'SMA15', 'SMA50', 'SMA100']
+    else:
+        frame = frame.loc[:,["Symbol", "OpenTime", "OpenPrice", "High", "Low", "ClosePrice", "CloseTime"]]
+        frame.columns = ['Symbol', 'OpenTime', 'OpenPrice', 'High', 'Low', 'ClosePrice', 'CloseTime']
     frame.OpenTime = pd.to_datetime(frame.OpenTime, unit='ms')
     frame.OpenPrice = frame.OpenPrice.astype(float)
     frame.High = frame.High.astype(float)
     frame.Low = frame.Low.astype(float)
     frame.ClosePrice = frame.ClosePrice.astype(float)
     frame.CloseTime = pd.to_datetime(frame.CloseTime, unit='ms')
-    frame.CandleSize = frame.CandleSize.astype(str)
+    if len(SMAs) > 0:
+        frame.SMA5 = frame.SMA5.astype(float)
+        frame.SMA15 = frame.SMA15.astype(float)
+        frame.SMA50 = frame.SMA50.astype(float)
+        frame.SMA100 = frame.SMA100.astype(float)
     return frame
 
+def findSMA(df, periods, SMAs = []):
+    title = "SMA" + str(periods)
+
+    if len(df) == len(SMAs):
+        df[title] = SMAs
+        return (df)
+
+    if len(df) < (periods * 1.5):
+        print ("not enough df to analyse!")
+        return
+
+    while (len(SMAs) < periods):
+        SMAs.append(0)
+
+    PricesSum = 0
+    for i in range (len(SMAs) - periods, (len(SMAs))):
+        PricesSum += df.ClosePrice[i]
+    PricesSum /= periods
+    SMAs.append(PricesSum)
+    return findSMA(df, periods, SMAs)
+
 if __name__ == "__main__":
-    engine = sqlalchemy.create_engine('sqlite:///BTCUSDTstream.db')
-    getHistoricalData1Day('BTCUSDT')
-    #getHistoricalData4Hours('BTCUSDT')
+    #getHistoricalData1Day('BTCUSDT')
+    getHistoricalData4Hours('BTCUSDT')
+    df = pd.read_sql('BTCUSDT', engine)
+    df = df.iloc[:]
+    print (df)
+
     loop = asyncio.get_event_loop()
-    #loop.create_task(getCurrentData4Hours('BTCUSDT'))
-    loop.create_task(getCurrentData1Day('BTCUSDT'))
+    loop.create_task(getCurrentData4Hours('BTCUSDT', df))
+    #loop.create_task(getCurrentData1Day('BTCUSDT'))
     loop.run_forever()
--- Main.py
+++ Main.py
@@ -2,98 +2,76 @@ from binance.client import Client
 from binance import BinanceSocketManager
 import matplotlib
 from matplotlib import markers
+from matplotlib import animation
 from matplotlib.pyplot import hlines
 import pandas as pd
 import sqlalchemy
 import mplfinance as mpl
+import matplotlib.animation as animation
+import asyncio
 
 EMA_lenght = 10
+global ifGoldenCross
 
-def findLowestPrice(amount, data):
-    lowest = data.ClosePrice[0]
+
+
+def findLowestPrice(amount, df):
+    lowest = df.ClosePrice[0]
     index = 0
     for i in range (0, amount):
-        #print (i, ": ", data.ClosePrice[i])
-        if data.ClosePrice[i] < lowest:
-            lowest = data.ClosePrice[i]
+        #print (i, ": ", df.ClosePrice[i])
+        if df.ClosePrice[i] < lowest:
+            lowest = df.ClosePrice[i]
             index = i
     return index
 
-def findHighestPrice(amount, data):
-    highest = data.ClosePrice[0]
+def findHighestPrice(amount, df):
+    highest = df.ClosePrice[0]
     index = 0
     for i in range (1, amount - 1):
-        if data.ClosePrice[i] > highest:
-            highest = data.ClosePrice[i]
+        if df.ClosePrice[i] > highest:
+            highest = df.ClosePrice[i]
             index = i
     return index
 
-def findLower(index, data):
-    lower = data.ClosePrice[index]
-    newIndex = index
-    amount = 10
-    if (len(data) - index) < 10:
-        amount = len(data) - index
-    for i in range (index, index + amount):
-        if data.ClosePrice[i] < lower:
-            lower = data.ClosePrice[i]
-            newIndex = i
-    if newIndex == index:
-        return -1
-    return newIndex
-
-def findHigher(index, data):
-    higher = data.ClosePrice[index]
-    newIndex = index
-    amount = 10
-    if (len(data) - index) < 10:
-        amount = len(data) - index
-    for i in range (index, index + amount):
-        if data.ClosePrice[i] > higher:
-            higher = data.ClosePrice[i]
-            newIndex = i
-    if newIndex == index:
-        return -1
-    return newIndex
-
-def findEMA(data, EMAs = []):
+def findEMA(df, EMAs = []):
     priceSum = 0
     while len(EMAs) < 11:
         EMAs.append(0)
-        priceSum += data.ClosePrice[len(EMAs)]
+        priceSum += df.ClosePrice[len(EMAs)]
 
     multiplier = (2 / (EMA_lenght + 1))
 
 
 
-    if (len(EMAs) == len(data)):
-        data['EMA'] = EMAs
-        return (data)
+    if (len(EMAs) == len(df)):
+        df['EMA'] = EMAs
+        return (df)
     if (not EMAs):
         PricesSum = 0
         for i in range (0, EMA_lenght):
-            PricesSum += data.ClosePrice[i]
+            PricesSum += df.ClosePrice[i]
             if i < (EMA_lenght - 1):
                 EMAs.append(0)
         PricesSum /= EMA_lenght
         EMAs.append(PricesSum)
-        return findEMA(data, EMAs)
+        return findEMA(df, EMAs)
 
     else:
-        currentPrice = data.ClosePrice[len(EMAs)]
+        currentPrice = df.ClosePrice[len(EMAs)]
         EMA = (currentPrice * multiplier) + (EMAs[len(EMAs) - 1] * (1 - multiplier))
         EMAs.append(EMA)
-        return findEMA(data, EMAs)
+        return findEMA(df, EMAs)
 
-def findSMA(data, periods, SMAs = []):
+def findSMA(df, periods, SMAs = []):
     title = "SMA" + str(periods)
 
-    if (len(data) == len(SMAs)):
-        data[title] = SMAs
-        return (data)
+    if (len(df) == len(SMAs)):
+        df[title] = SMAs
+        return (df)
 
-    if len(data) < (periods * 1.5):
-        print ("not enough data to analyse!")
+    if len(df) < (periods * 1.5):
+        print ("not enough df to analyse!")
         return
 
     while (len(SMAs) < 100):
@@ -101,49 +79,60 @@ def findSMA(data, periods, SMAs = []):
 
     PricesSum = 0
     for i in range (len(SMAs) - periods, (len(SMAs))):
-        PricesSum += data.ClosePrice[i]
+        PricesSum += df.ClosePrice[i]
     PricesSum /= periods
     SMAs.append (PricesSum)
-    return findSMA (data, periods, SMAs)
+    return findSMA (df, periods, SMAs)
 
-def findDEMA(data, DEMAs = []):
+def findDEMA(df, DEMAs = []):
     pass
     #TODO
 
-def findCross(data):
-    flag = False
+def findCross(df, short = False):
+    lineSmall = []
+    lineBig = []
+    SMASmall = 0
+    SMABig = 0
+    if short:
+        lineSmall = df.SMA5
+        lineBig = df.SMA15
+        SMASmall = 5
+        SMABig = 15
+    else:
+        lineSmall = df.SMA50
+        lineBig = df.SMA100
+        SMASmall = 50
+        SMABig = 100
+    ifGoldenCross = False
     ifCrossed = False
-    if data.SMA100[0] > data.SMA50[0]:
-        flag = True
+    if lineBig[0] < lineSmall[0]:
+        ifGoldenCross = True
 
-    for i in range (1, len(data) - 1):
-        #print (i)
-        if data.SMA100[i] < data.SMA50[i] and flag == True:
-            print ("SMA100 and SMA50 croossed around", data.CloseTime[i])
-            flag = False
+    for i in range (1, len(df) - 1):
+        if lineBig[i] > lineSmall[i] and ifGoldenCross == True:
+            print ("There was a death cross at around ", df.CloseTime[i], "- price should go down soon")
+            ifGoldenCross = False
             ifCrossed = True
-            #return True
-        elif data.SMA100[i] > data.SMA50[i] and flag == False:
-            print ("SMA100 and SMA50 croossed around", data.CloseTime[i])
-            flag = True
+        elif lineBig[i] < lineSmall[i] and ifGoldenCross == False:
+            print ("There was a golden cross at around ", df.CloseTime[i], "- price should go up soon")
+            ifGoldenCross = True
             ifCrossed = True
-            #return True
     
     if ifCrossed:
         return False
-    print ("SMA100 and SMA50 have't crossed for the last", len(data) - 1, "days")
+    print ("SMA100 and SMA50 have't crossed for the last", len(df) - 1, "days")
 
-def biggestTrendFibonacci(data):
-    highest = findHighestPrice(len(data), data)
-    lowest = findLowestPrice(len(data), data)
+def biggestTrendFibonacci(df):
+    highest = findHighestPrice(len(df), df)
+    lowest = findLowestPrice(len(df), df)
 
-    priceDifferential = data.ClosePrice[highest] - data.ClosePrice[lowest]
-    firstLine = data.ClosePrice[highest]
-    secondLine = data.ClosePrice[highest] - ((priceDifferential / 100) * 23.6)
-    thirdLine = data.ClosePrice[highest] - ((priceDifferential / 100) * 38.2)
-    forthLine = data.ClosePrice[highest] - ((priceDifferential / 100) * 61.8)
-    fifthLine = data.ClosePrice[highest] - ((priceDifferential / 100) * 78.6)
-    sixthLine = data.ClosePrice[lowest]
+    priceDifferential = df.ClosePrice[highest] - df.ClosePrice[lowest]
+    firstLine = df.ClosePrice[highest]
+    secondLine = df.ClosePrice[highest] - ((priceDifferential / 100) * 23.6)
+    thirdLine = df.ClosePrice[highest] - ((priceDifferential / 100) * 38.2)
+    forthLine = df.ClosePrice[highest] - ((priceDifferential / 100) * 61.8)
+    fifthLine = df.ClosePrice[highest] - ((priceDifferential / 100) * 78.6)
+    sixthLine = df.ClosePrice[lowest]
 
     return [sixthLine, fifthLine, forthLine, thirdLine, secondLine, firstLine]
 
@@ -154,18 +143,16 @@ def deltaLines(lines = []):
         upLine = lines[currentInterval]
         downLine = lines[currentInterval - 1]
         delta = (upLine - downLine) * 5 / 100
-        dLines.append(upLine - delta)
         dLines.append(downLine + delta)
-        #dLines.append(downLine)
-    #dLines.append(lines[5])
+        dLines.append(upLine - delta)
     return dLines
 
-def isCloseToLines(data):
-    lines = biggestTrendFibonacci(data)
+def isCloseToLines(df):
+    lines = biggestTrendFibonacci(df)
     isClose = False
     changedInterval = 0
 
-    price = data.ClosePrice[0]
+    price = df.ClosePrice[0]
 
     currentInterval = 0
     for j in lines:
@@ -173,11 +160,11 @@ def isCloseToLines(data):
             currentInterval += 1
 
 
-    for i in range (0, (len(data) - 1)):
+    for i in range (0, (len(df) - 1)):
         upLine = lines[currentInterval]
         downLine = lines[currentInterval - 1]
         delta = (upLine - downLine) * 5 / 100
-        price = data.ClosePrice[i]
+        price = df.ClosePrice[i]
 
         if currentInterval < 1 or currentInterval > 5:
             print ("Error")
@@ -206,81 +193,80 @@ def isCloseToLines(data):
         else:
             if isClose:
                 isClose = False
-                print ("current price is not close to either of the lines")
-                if changedInterval == 0:
-                    print("The line broke")
-                elif changedInterval > 0:
+                print ("current price is not close to either of the lines", i)
+                if changedInterval > 0:
                     print("bounce up")
                 else:
                     print("bounce down")
                 changedInterval = False
-        print(data.ClosePrice[i], i)
+            print("nothing happened", i)
 
-def displayCharts(data):
-    matplotlib.use('TkAgg')
-    newData = data.drop(['CloseTime', 'Symbol', 'OpenTime'], axis = 1, inplace = False)
-    newData.index = pd.DatetimeIndex(data['OpenTime'])
-    newData.rename(columns = {'OpenPrice': 'Open', 'High': 'High', 'Low': 'Low', 'ClosePrice': 'Close'}, inplace = True)
-    #EMA = mpl.make_addplot(newData["EMA"].values, panel = 0, color = 'fuchsia')
+
+def displayCharts(df):
+    #matplotlib.use('TkAgg')
+    newDF = df.drop(['CloseTime', 'Symbol', 'OpenTime'], axis = 1, inplace = False)
+    newDF.index = pd.DatetimeIndex(df['OpenTime'])
+    newDF.rename(columns = {'OpenPrice': 'Open', 'High': 'High', 'Low': 'Low', 'ClosePrice': 'Close'}, inplace = True)
+    #EMA = mpl.make_addplot(newDF["EMA"].values, panel = 0, color = 'fuchsia')
     #floatEMA = []
-    #for i in range (0, len(data) - EMA_lenght):
-    #    floatEMA.append(int(data.EMA[i + EMA_lenght]))
+    #for i in range (0, len(df) - EMA_lenght):
+    #    floatEMA.append(int(df.EMA[i + EMA_lenght]))
 
     #TODO display the EMA
 
-    lines = biggestTrendFibonacci(data)
+    lines = biggestTrendFibonacci(df)
     dlines = deltaLines(lines)
     alphaLines = []
     for i in range(0, 6):
-        alphaLines.append([(data.OpenTime[0], lines[i]), (data.OpenTime[len(newData) - 1], lines[i])])
+        alphaLines.append([(df.OpenTime[0], lines[i]), (df.OpenTime[len(newDF) - 1], lines[i])])
     for i in range(0, 10):
-        alphaLines.append([(data.OpenTime[0], dlines[i]), (data.OpenTime[len(newData) - 1], dlines[i])])
+        alphaLines.append([(df.OpenTime[0], dlines[i]), (df.OpenTime[len(newDF) - 1], dlines[i])])
     alphaLines.sort()
 
-    mpl.plot(newData, type = 'candle', style = 'charles', title = 'BTC Price', mav = (50, 100), alines = dict(alines=alphaLines,
+    mpl.plot(newDF, type = 'candle', style = 'charles', title = 'BTC Price', mav = (5, 15), alines = dict(alines=alphaLines,
                                                                                                               colors=['blue', 'red', 'red'],
                                                                                                               linewidths = 0.5))
     mpl.show()
 
+def animate(ival, df):
+    mpl.plot(df, type='candle', style = 'charles')
+
+async def main(df):
+    while True:
+        await asyncio.sleep(60)
+        dataSize = len(df)
+        SMA5 = 0
+        SMA15 = 0
+        SMA50 = 0
+        SMA100 = 0
+        for i in range (dataSize - 1, dataSize - 101):
+            if i > dataSize - 6:
+                SMA5 += df.ClosePrise[i]
+            if i > dataSize - 16:
+                    SMA15 += df.ClosePrise[i]
+            if i > dataSize - 51:
+                SMA50 += df.ClosePrise[i]
+            SMA100 += df.ClosePrise[i]
+        df.SMA5[dataSize - 1] = SMA5 / 5
+        df.SMA15[dataSize - 1] = SMA15 / 15
+        df.SMA50[dataSize - 1] = SMA50 / 50
+        df.SMA100[dataSize - 1] = SMA100 / 100
+        print (df)
+
 if __name__ == "__main__":
     engine = sqlalchemy.create_engine('sqlite:///BTCUSDTstream.db')
     df = pd.read_sql('BTCUSDT', engine)
-    data = df.iloc[:]
-    data = findEMA(data, [])
-    data = findSMA(data, 100, [])
-    data = findSMA(data, 50, [])
-    #print (data)
-    #findCross(data)
-    isCloseToLines(data)
-    displayCharts(data)
-
-    #print(findLower(findLowestPrice(5, data), data))
-
-    #for i in range (0, len(data) - 1):
-    #    if data.OpenPrice[i] > data.ClosePrice[i]:
-    #        print (data.Symbol[i], " is down ", data.OpenPrice[i] - data.ClosePrice[i], " in the last minute")
-    #    elif data.OpenPrice[i] < data.ClosePrice[i]:
-    #        print (data.Symbol[i], " is up ", data.ClosePrice[i] - data.OpenPrice[i], " in the last minute")
-    #    elif data.OpenPrice[i] == data.ClosePrice[i]:
-    #        print (data.Symbol[i], "'s price hasn't moved in the last minute")
-            
+    df = df.iloc[:]
+    df = findEMA(df, [])
+    df = findSMA(df, 100, [])
+    df = findSMA(df, 50, [])
+    df = findSMA(df, 15, [])
+    df = findSMA(df, 5, [])
+    print (df)
+    findCross(df, False)
+    displayCharts(df)
+    loop = asyncio.get_event_loop()
+    loop.create_task(df = df.iloc[1: , :])
+    loop.create_task(main(df))
+    loop.run_forever()
     
-    #lowest = data.Low[1]
-    #highest = data.High[1]
-    #lowestClose = data.ClosePrice[1]
-    #highestClose = data.ClosePrice[1]
-    #for i in range (2, 5):
-    #    if data.Low[i] < lowest:
-    #        lowest = data.Low[i]
-    #    if data.High[i] > highest:
-    #        highest = data.High[i]
-    #   if data.ClosePrice[i] < lowestClose:
-    #        lowestClose = data.ClosePrice[i]
-    #    if data.ClosePrice[i] > highestClose:
-    #        highestClose = data.ClosePrice[i]
-
-    #print("lowest: ", lowest)
-    #print("highest: ", highest)
-    #print("lowestClose: ", lowestClose)
-    #print("HighestClose: ", highestClose)
-    #print (data)
